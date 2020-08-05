66cb1e3adbac1b7f139808336b32afd39bfae675
Patrick Nymark <patricknymark24@gmail.com>
2020-08-05 02:57:13 +0200

    initial commit

--- SensorProject/ViewController.swift
+++ SensorProject/ViewController.swift
@@ -7,14 +7,45 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    var locationManager = CLLocationManager()
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        mapView.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
+        let geofenceRegionCenter = CLLocationCoordinate2DMake(37.33233141, -122.0312186)
+        let geofenceRegion = CLCircularRegion(center: geofenceRegionCenter, radius: 100, identifier: "notifymeonExit")
+        geofenceRegion.notifyOnExit = true
+        geofenceRegion.notifyOnEntry = true
+        
+        locationManager.startMonitoring(for: geofenceRegion)
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLDistanceFilterNone
+        locationManager.startUpdatingLocation()
+        
+        mapView.showsUserLocation = true
+        
+        //Zoom to user location
+        if let userLocation = locationManager.location?.coordinate {
+            let viewRegion = MKCoordinateRegion(center: userLocation, latitudinalMeters: 1500, longitudinalMeters: 1500)
+            mapView.setRegion(viewRegion, animated: false)
+        }
     }
+}
 
-
+extension ViewController: MKMapViewDelegate, CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        print("entered region")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
+        print("exited region")
+    }
 }
 
